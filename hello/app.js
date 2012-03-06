1262c39dbbe21cb53c1b7d23b0ca3b45e7896ba1
doortts <doortts@gmail.com>
2012-03-06 18:50:41 +0900

    bootstrap start

--- hello/app.js
+++ hello/app.js
@@ -1,10 +1,10 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes');
+	, routes = require('./routes')
+	, socket = require('socket.io');
 
 var app = module.exports = express.createServer();
 
@@ -28,8 +28,38 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(8888);
+var lastConnectedId = 0;
+var io = socket.listen(app);
+io.set('log level', 2);
+
+io.sockets.on('connection', function(socket) {
+	var name;
+
+	socket.on('setname', function (data) {
+		socket.set('name', data);
+		name = data;
+		socket.emit('responsename', name);
+	});
+
+	socket.on('getname', function(error, data) {
+		socket.emit('responsename', name);
+	});
+
+	socket.on('join', function(data) {
+		socket.join(data);
+		socket.set('room', data);
+	});
+
+	socket.on('message', function(data) {
+		socket.get('room', function(error, room) {
+			io.sockets.in(room).emit('message', data);
+		})
+	});
+});
+
+
+
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
